5bb401635e3eb8d1ba0d976036d1c9a23e78c6c4
Yevheniy Savenkov <yevheniy.savenkov@gmail.com>
2021-10-15 17:39:28 +0300

    message changed to <Hello, mates!>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // 1 2 3 GO
+        System.out.println("Hello, mates!");
+
 
     }
 }
